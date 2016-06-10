117cb52eb9810d77a9acc057867aa3d6b76b5647
phillinetan <elizamei12@yahoo.com.ph>
2016-06-10 17:48:05 +0800

    added library
    sample java code

---
+++ Library.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Student{
+	String name;
+	Student(String name){
+		this.name=name+'!';
+	}
+
+}
+public class Library{
+	public String name;
+	public Random rand;
+	Student[] students= new Student[6];
+	int index;
+	Scanner input=new Scanner(System.in);
+	Library(String name){
+		this.name=name;
+		index=0;
+		rand=new Random();
+	}
+	private void get(){
+		char n=input.next().charAt(0);
+		System.out.println(n);
+		
+	}
+	public static void main(String[] args){
+		Library l= new Library(args[0]);
+		l.get();
+	}
+
+
+}
+//hotarubi no mori e
